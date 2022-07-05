3045c230fc47ccab608ab49a7a579480c72c0825
Loan <loan.matheus@hotmail.com>
2022-07-05 00:28:02 -0300

    fix: Correção de lint do comit anterior

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext } from 'react';
 import ContextDefault from '../../Context/Context';
 import Header from '../../Components/Header/Header';
 import AOS from 'aos';
